f0dc25dfdfd6ca172d31ee8d8b57f392c9ca0952
William Das <williamhdas@gmail.com>
2023-12-17 23:29:45 -0500

    dermai logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,8 @@ export default function Home() {
             <Link href="" className={getTabClass(activeTab)} onClick={() => setActiveTab('home')}>
                 <svg
                   className=" h-6 w-6"
-                  fill="none"
+                  fill="pink"
+                  color="white"
                   height="24"
                   stroke="currentColor"
                   strokeLinecap="round"
@@ -55,7 +56,7 @@ export default function Home() {
                 >
                   <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
                 </svg>
-                <span className="">DermAssist+</span>
+                <span className="">DermAI+</span>
               </Link>
           </div>
           <div className="flex-1 overflow-auto py-2">
